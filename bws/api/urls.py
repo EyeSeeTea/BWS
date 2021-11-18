a2e2ab23229784d12d82dfe7ea9029321b6c1fe5
Jose R Macias <jr.macias@cnb.csic.es>
2021-11-18 18:58:12 +0100

    Add first endpoint to ws api

---
+++ bws/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path, re_path
+from rest_framework.routers import DefaultRouter
+from api import views
+
+router = DefaultRouter()
+router.register(r'datafiles', views.EntryViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+    # EM Validation annotations for 3DBionotes - Protvista 
+    re_path(r'^pdbAnnotFromMap/all/(?P<pdb_id>\d\w{3})/(?P<chain_id>\w{1})/?(?P<modified_model>(pdb-redo|isolde))?/$', views.PdbEntryAllAnnFromMapView.as_view()),
+]
