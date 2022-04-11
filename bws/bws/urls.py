8f4e02671f8006ed3efa35d1b03bc25dc4f53450
Jose R Macias <jr.macias@cnb.csic.es>
2022-04-11 17:19:09 +0200

    Add Swagger UI documentation.

--- bws/bws/urls.py
+++ bws/bws/urls.py
@@ -2,17 +2,37 @@
     bws URL Configuration
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers, serializers, viewsets
+from django.urls import include, path, re_path
+from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from bws.settings import *
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="EM-Validations API",
+        default_version=REST_FRAMEWORK['DEFAULT_VERSION'],
+        description="API documentation for the Cryo-EM Validation annotation WebServices",
+        contact=openapi.Contact(email="3dbionotes@cnb.csic.es"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
-    path('', include(router.urls)),
+    re_path(r'^api/swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^api/swagger/$', schema_view.with_ui('swagger',
+                                                   cache_timeout=0), name='schema-swagger-ui'),
+
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
