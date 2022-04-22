4352dd739b35c920218d692a214b7042f45b64fd
Jose R Macias <jr.macias@cnb.csic.es>
2022-04-22 20:13:25 +0200

    Fix problem to run Django managment outside Docker
    During development, usually it is util to run management commands
    outside the Docker containers, directly from the shared folders
    as volumes

--- bws/bws/settings/settings.py
+++ bws/bws/settings/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
