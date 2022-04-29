34d2f0c4349f0173e92a1fde1b4e935265d41ade
Jose R Macias <jr.macias@cnb.csic.es>
2022-04-29 19:44:54 +0200

    Remove copy-pasted code

--- bws/api/management/commands/update_Isolde_Refinements.py
+++ bws/api/management/commands/update_Isolde_Refinements.py
@@ -8,13 +8,8 @@ from api.utils import update_isolde_refinements
 
 class Command(BaseCommand):
     """
-    Convert EM Validations to FunPDBe Data Exchange Format (JSON)
+    Command to update the Isolde data from GitHub:
     """
-
-    help = "Convert EM Validations to FunPDBe Data Exchange Format (JSON) from <path>"
-    missing_args_message = "Too few arguments. Please, provide a folder name for the data files."
-    requires_migrations_checks = True
-
     help = "Update Isolde refinements from GitHub"
     missing_args_message = "Too few arguments. Please, provide a filename."
     requires_migrations_checks = True
