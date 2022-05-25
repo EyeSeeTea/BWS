bb8cfa8e9c821a5a50a3e073a3031ff178122245
csimong <carolina.simon@cnb.csic.es>
2022-05-25 10:57:35 +0200

    Remove pagination = 10 records

--- bws/bws/settings/settings.py
+++ bws/bws/settings/settings.py
@@ -169,7 +169,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
 }
 
 SWAGGER_SETTINGS = {
