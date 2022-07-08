18c11988d103baef30a3e9359fb74742f53aa6fe
csimong <carolina.simon@cnb.csic.es>
2022-07-08 12:09:13 +0200

    Register models in admin site

--- bws/api/admin.py
+++ bws/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from api import models
 
 # Register your models here.
+
+admin.site.register(models.FeatureType)
+#admin.site.register(models.StudyToOrganism)
+admin.site.register(models.StudyEntity)
+admin.site.register(models.ScreenEntity)
+admin.site.register(models.PlateEntity)
+admin.site.register(models.WellEntity)
+#admin.site.register(models.PublicationAuthor)
+admin.site.register(models.Publication)
+admin.site.register(models.Author)
+admin.site.register(models.LigandEntity)
+admin.site.register(models.PdbToLigand)
+admin.site.register(models.Organism)
+admin.site.register(models.PdbEntry)
