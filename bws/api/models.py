f687b2154f60fbbdbfd794cfc01650b3c66342ae
Carolina Simón Guerrero <carolina.simon@cnb.csic.es>
2023-03-07 20:06:02 +0100

    Add default to units in Analyses

--- bws/api/models.py
+++ bws/api/models.py
@@ -614,7 +614,8 @@ class Analyses(models.Model):
     value = models.FloatField(null=False, blank=False, default=0)
     description = models.CharField(
         max_length=255, blank=True, null=True, default='')
-    units = models.ForeignKey(OntologyTerm, related_name='unit_term_analyses', on_delete=models.CASCADE)
+    units = models.ForeignKey(OntologyTerm, default='',
+                              related_name='unit_term_analyses', on_delete=models.CASCADE)
     pvalue = models.FloatField(null=True, blank=True)
     dataComment = models.CharField(
         max_length=255, blank=True, null=True, default='')
