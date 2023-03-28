701f573681154e276a025a22fb610bab1094a151
Carolina Simón Guerrero <carolina.simon@cnb.csic.es>
2023-03-28 11:42:50 +0200

    Add PTMEntity and DomainEntity models and update FeatureEntity relations to M2M

--- bws/api/models.py
+++ bws/api/models.py
@@ -447,8 +447,40 @@ class PdbEntryDetails(models.Model):
                                related_name='details', on_delete=models.CASCADE)
 
     refdoc = models.ManyToManyField(Publication)
+    
+class PTMEntity(models.Model):
+    '''
+    Protein entities that arise from other proteins' post-translational modifications (PTMs) and/or 
+    processing events and do not have UniProt id associated.
+    '''
+    name = models.CharField(max_length=255, blank=False,
+                            null=False, default='')
+    description = models.CharField(
+        max_length=255, blank=True, null=True, default='')
+    start = models.IntegerField()
+    end = models.IntegerField()
+    uniprotentry = models.ForeignKey(UniProtEntry,
+                              related_name='PTMs', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '%s' % (self.name)
+    
+class DomainEntity(models.Model):
+    '''
+    Domain or region of interest for a specific protein
+    '''
+    name = models.CharField(max_length=255, blank=False,
+                            null=False, default='')
+    description = models.CharField(
+        max_length=255, blank=True, null=True, default='')
+    start = models.IntegerField()
+    end = models.IntegerField()
+    uniprotentry = models.ForeignKey(UniProtEntry,
+                              related_name='domains_regions', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '%s' % (self.name)
+    
 class FeatureType(models.Model):
     '''
         Feature type
@@ -472,9 +504,13 @@ class FeatureEntity(models.Model):
     description = models.CharField(max_length=5000, blank=True, null=True)
     pdbentry = models.ForeignKey(PdbEntry,
                                  related_name='%(class)s_features', null=True, blank=True, on_delete=models.CASCADE)
-    uniprotentry = models.ForeignKey(UniProtEntry,
+    uniprotentry = models.ManyToManyField(UniProtEntry,
                                  related_name='%(class)s_features', null=True, blank=True, on_delete=models.CASCADE)
-    ligandentity = models.ForeignKey(LigandEntity,
+    ligandentity = models.ManyToManyField(LigandEntity,
+                                 related_name='%(class)s_features', null=True, blank=True, on_delete=models.CASCADE)
+    ptmentity = models.ManyToManyField(PTMEntity,
+                                 related_name='%(class)s_features', null=True, blank=True, on_delete=models.CASCADE)
+    domainentity = models.ManyToManyField(DomainEntity,
                                  related_name='%(class)s_features', null=True, blank=True, on_delete=models.CASCADE)
     externalLink = models.URLField(max_length=200, default='', blank=True)
 
