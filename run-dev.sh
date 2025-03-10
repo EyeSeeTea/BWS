d8a0daead5d82f3b028ce00c61fa2a7002842d91
p3rcypj <pablo@percypj.com>
2025-03-10 01:45:31 +0000

    Refactor env variables

--- run-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Launching 3DBionotes-WS API (BWS) in development mode"
-
-docker compose --env-file .env-dev -f docker-compose.develop.yml up -d
--- run-prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Launching 3DBionotes-WS API (BWS) in PRODUCTION mode"
-
-docker compose --env-file .env-prod -f docker-compose.production.yml up -d
