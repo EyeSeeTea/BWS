1f080e15cdd9c4737eece4a2aac7b9e65a54d469
p3rcypj <pablo@percypj.com>
2025-10-14 13:06:12 +0000

    Entrypoint make migrations

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Starting http server'
-python manage.py makemigrations &&
+python manage.py makemigrations api && # Probably necessary to also add annotations also to makemigrations (as new line)
 python manage.py migrate &&
 python manage.py rebuild_index --noinput &&
 uwsgi --module bws.wsgi:application --http :8000 --master --enable-threads
